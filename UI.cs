822ba7622257785f1ec4a92804c7a4cec4e59062
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-11 09:11:10 +1000

    Add files via upload

---
+++ UI.cs
@@ -0,0 +1,12 @@
+string Filename = @"UI.cs"
+public class ui;
+class ui;
+{
+	//Windows type = os
+	//Windows UI optimized
+	//Windows UI repaired icons
+	//Windows UI repaired toolbar
+	//Windows UI repaired performance
+	//Windows UI repaired optimization
+}
+
