26bb43fd78a0be716c4c98ef4e4a9a9047d3cf88
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-11 09:37:53 +1000

    Add files via upload

---
+++ physicalmemory.cs
@@ -0,0 +1,13 @@
+string Filename = @"physicalmemory.cs"
+public class physicalmemory;
+class physicalmemory;
+{
+	//Windows type os
+	//Windows physical memory optimized
+	//Windows physical memory repaired performance
+	//Windows physical memory repaired optimization
+	//Windows physical memory repaired load
+}
+
+
+
