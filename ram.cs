c886df584ec1d5399d35ba58c345f3b2f33a4692
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-11 09:39:56 +1000

    Add files via upload

---
+++ ram.cs
@@ -0,0 +1,13 @@
+string Filename = @"ram.cs"
+public class ram;
+class ram;
+{
+	//Windows type os
+	//Windows RAM optimized
+	//Windows RAM repaired performance
+	//Windows RAM repaired optimization
+	//Windows RAM repaired load
+}
+
+
+
