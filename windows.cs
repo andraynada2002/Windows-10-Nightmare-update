c58aa4a846d987334a98286afc7f213470a81adc
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-14 15:33:55 +1000

    Add files via upload

---
+++ windows.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Андрей
+ * Date: 14.04.2019
+ * Time: 14:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+//Windows type os
+//Windows system optimized
+void reload();
+void load();
+
+//Windows fixed performance
+void performance();
+void performancehight();
+
+//Windows Wi-Fi optimized
+void wifi();
+
+//Windows Net adapter traced
+void net_adapter();
+void net_adapter_traced();
+
+//Windows performance traced
+void performance_traced();
+
+//Windows ray tracing on
+void ray_tracing();
+
+//debug
+void debug();
+
+//Windows DirectX 11.0 raytraced
+void directx11_1();
+void directx11_2();
+void directx11_3();
+void raytraced();
+
+//Windows DirectX 12.0 raytraced
+namespace DX12();
+{
+	void raytraced();
+	void directx12_0();
+}
+
+//Windows DirectX 12.1 raytraced
+namespace DX121();
+{
+	void raytraced();
+	void directx12_1();
+}
+
+//Windows GPU performance hight
+namespace gpu();
+{
+	void GPU();
+	void performance();
+}
+
+//Windows DirectX 9.0 raytraced
+namespace DirectX9();
+{
+	void directx9_0c();
+	void directx9_0a();
+	void directx9_0b();
+	void raytraced();
+}
+
+//Windows DirectX 10_0
+namespace dx11();
+{
+	void directx10_0();
+	void directx10_1();
+	void directx10_2();
+	void directx10_3();
+	void raytraced();
+}
+
+//Windows CPU performance hight
+namespace CPU();
+{
+	void CPU;
+	void performance();
+}
+
+//Windows RAM performance hight
+namespace RAM();
+{
+	void RAM();
+	void Performance();
+}
+
+//Windows Physical Memory performance hight
+namespace Physicalmemory();
+{
+	void physical_memory();
+	void performance();
+}
+
+//Windows DirectX path traced
+namespace path();
+{
+	void directx();
+	void pathtraced();
+}
+
+//Windows performance hight
+namespace pe()
+{
+	void windows();
+	void performance();
+}
+
+//Windows core optimized
+namespace core();
+{
+	void windows():
+	void core():
+}
+
+//Windows core moderned
+namespace core2();
+{
+	void windows();
+	void core();
+	void moderned();
+}
+
+//Windows shell optimized
+namespace shell();
+{
+	void windows();
+	void shell();
+}
+
+//Windows shell moderned
+namespace shell2();
+{
+	void windows();
+	void shell();
+	void moderned();
+}
+
