f5d5acff756f68edc1359be5ca88d89128b0f219
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-22 20:40:16 +1000

    Add files via upload

---
+++ wific.cs
@@ -0,0 +1,23 @@
+class wific;
+public class wific;
+private class wific;
+
+//Wifi optimized data
+//Wifi optimized speed
+//Wifi optimized ping
+//Wifi optimized bandwidth
+//Wifi traced data
+//Wifi traced signal
+namespace wific;
+{
+	void data()
+	void wifi()
+	public void wifi()
+	private void wifi()
+	int 59;
+	public int 58;
+	private int 57;
+	protected wifi()
+	public protected wifi()
+	private protected wifi()
+}
