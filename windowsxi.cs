34e17ebc0f992a6b37f297f5e498dafbd6323df9
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-22 21:22:55 +1000

    Add files via upload

---
+++ windowsxi.cs
@@ -0,0 +1,68 @@
+class windowsxi;
+public class windowsxi;
+private class windowsxi;
+
+//Windows 11 type os
+//Windows 11 type windows 10
+//Windows 11 type windows operating system
+//Windows 11 optimized
+//Windows 11 optimized core
+//Windows 11 optimized shell
+//Windows 11 optimized CPU cores
+//Windows 11 optimized GPU cores
+//Windows 11 optimized subsurface
+//Windows 11 optimized desktop
+//Windows 11 optimized applications
+//Windows 11 optimized standalone applications
+namespace windowsxi;
+{
+	void windowsxi()
+	void optimization()
+	void windows11()
+	void core()
+	void shell()
+	public void windowsxi()
+	private void windowsxi()
+	int 1;
+	public int 2;
+	private int 3;
+	protected windowsxi()
+	public protected windowsxi()
+	private protected windowsxi()
+	object array 20;
+	public object array 21;
+	private object array 22;
+	float 1;
+	public float 2;
+	private float 3;
+	bool 0;
+	public bool 2;
+	private bool 3;
+	sbyte 59;
+	public sbyte 58;
+	private sbyte 53;
+	byte 2048;
+	public byte 4096;
+	private byte 8192;
+	object get 0x2;
+	object get 0x3;
+	object get 0x1;
+	public object get 0x4;
+	private object get 0x6
+	double 2;
+	public double 3;
+	private double 4;
+	params windowsxi;
+	public params windowsxi;
+	private params windowsxi;
+}
+
+
+
+
+
+
+
+
+
+
